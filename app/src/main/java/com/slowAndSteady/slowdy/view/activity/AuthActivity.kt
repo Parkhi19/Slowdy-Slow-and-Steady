47586dba11bce503f23deec34d2b59a69ac2540a
Parkhi <parkhigoyal46@gmail.com>
2023-06-23 13:11:24 +0530

    added auth activity

---
+++ app/src/main/java/com/slowAndSteady/slowdy/view/activity/AuthActivity.kt
@@ -0,0 +1,14 @@
+package com.slowAndSteady.slowdy.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.slowAndSteady.slowdy.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+    }
+}
