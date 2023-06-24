db4300770280e6d9e8a0a93e19bb951e64e30b55
Parkhi <parkhigoyal46@gmail.com>
2023-06-24 15:51:37 +0530

    added logIn navigation

---
+++ app/src/main/java/com/slowAndSteady/slowdy/view/fragments/auth/LandingFragment.kt
@@ -0,0 +1,35 @@
+package com.slowAndSteady.slowdy.view.fragments.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.slowAndSteady.slowdy.R
+import com.slowAndSteady.slowdy.databinding.FragmentLandingBinding
+import com.slowAndSteady.slowdy.databinding.FragmentSigninBinding
+
+
+class LandingFragment : Fragment() {
+
+    private lateinit var binding: FragmentLandingBinding
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLandingBinding.inflate(inflater, container, false)
+        navController = NavHostFragment.findNavController(this)
+
+        binding.loginButton.setOnClickListener {
+            // Handle the button click event here
+            val action = LandingFragmentDirections.actionLandingFragmentToSignInFragment()
+            navController.navigate(action)
+        }
+
+        return binding.root
+    }
+}
