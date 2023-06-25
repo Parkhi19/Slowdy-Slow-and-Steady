811eb0e14ce6e5612a81b40f5b5ee47318901373
Parkhi <parkhigoyal46@gmail.com>
2023-06-25 18:06:18 +0530

    added google sign in

--- app/src/main/java/com/slowAndSteady/slowdy/view/fragments/auth/SignInFragment.kt
+++ app/src/main/java/com/slowAndSteady/slowdy/view/fragments/auth/SignInFragment.kt
@@ -1,17 +1,30 @@
 package com.slowAndSteady.slowdy.view.fragments.auth
-
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.slowAndSteady.slowdy.R
 import com.slowAndSteady.slowdy.databinding.FragmentSigninBinding
 
 class SignInFragment : Fragment() {
-  private lateinit var binding: FragmentSigninBinding
-  private lateinit var navController:NavController
+    private lateinit var binding: FragmentSigninBinding
+    private lateinit var navController: NavController
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,14 +32,42 @@ class SignInFragment : Fragment() {
     ): View {
         binding = FragmentSigninBinding.inflate(inflater, container, false)
         navController = NavHostFragment.findNavController(this)
-//
-//        binding.button.setOnClickListener {
-//            // Handle the button click event here
-//            val action = SignInFragmentDirections.actionSignInFragmentToSignupFragment()
-//            navController.navigate(action)
-//        }
+
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), googleSignInOptions)
+        auth = Firebase.auth
+
+        binding.googleSignIn.setOnClickListener {
+            val signInClient = googleSignInClient.signInIntent
+            launcher.launch(signInClient)
+        }
 
         return binding.root
     }
 
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val resultObtained = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                if (resultObtained.isSuccessful) {
+                    val account: GoogleSignInAccount? = resultObtained.result
+                    val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+                    auth.signInWithCredential(credential).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(requireContext(), "Done!!", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(requireContext(), "Failed!!", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    // Handle sign-in failure
+                }
+            } else {
+                Toast.makeText(requireContext(), "Failed!!", Toast.LENGTH_SHORT).show()
+            }
+        }
 }
