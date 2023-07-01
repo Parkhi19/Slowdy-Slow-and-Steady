63c605189458bdec3b6b15bba0c6dc3e09a968a3
Parkhi <parkhigoyal46@gmail.com>
2023-07-01 20:36:32 +0530

    added setting fragment navigation

--- app/src/main/java/com/slowAndSteady/slowdy/view/fragments/home/SettingsFragment.kt
+++ app/src/main/java/com/slowAndSteady/slowdy/view/fragments/home/SettingsFragment.kt
@@ -5,18 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.slowAndSteady.slowdy.R
+import com.slowAndSteady.slowdy.databinding.FragmentSettingsBinding
+import com.slowAndSteady.slowdy.databinding.FragmentSignupBinding
 
 
 class SettingsFragment : Fragment() {
 
+    private lateinit var binding: FragmentSettingsBinding
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        navController = NavHostFragment.findNavController(this)
+
+        return binding.root
     }
 
 }
