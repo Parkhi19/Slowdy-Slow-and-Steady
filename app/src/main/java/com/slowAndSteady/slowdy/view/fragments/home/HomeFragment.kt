f3873ff498fc3a423848205139a48c361ee33153
Parkhi <parkhigoyal46@gmail.com>
2023-07-01 20:39:43 +0530

    added home fragment recycler view

--- app/src/main/java/com/slowAndSteady/slowdy/view/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/slowAndSteady/slowdy/view/fragments/home/HomeFragment.kt
@@ -1,20 +1,53 @@
 package com.slowAndSteady.slowdy.view.fragments.home
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.slowAndSteady.slowdy.R
+import com.slowAndSteady.slowdy.databinding.FragmentHomeBinding
+import com.slowAndSteady.slowdy.databinding.FragmentSigninBinding
+import com.slowAndSteady.slowdy.databinding.FragmentSignupBinding
+import com.slowAndSteady.slowdy.model.HabitModel
+import com.slowAndSteady.slowdy.utils.Utils
+import com.slowAndSteady.slowdy.view.adapter.HabitViewAdapter
+import com.slowAndSteady.slowdy.view.custom.HabitStreaks
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        binding.dateToday.text = Utils.getFormattedDate(Calendar.getInstance())
+        val habitLayoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        val habitLists = listOf(
+            HabitModel("1-11-1", "study", listOf(true, false, true, true, true, true, true, true, false, true, false, true, false)),
+            HabitModel("1-11-1", "study", listOf(true, false, true)),
+            HabitModel("1-11-1", "study", listOf(true, false, true)),
+            HabitModel("1-11-1", "study", listOf(true, false, true)),
+            HabitModel("1-11-1", "study", listOf(true, false, true)),
+            HabitModel("1-11-1", "study", listOf(true, false, true)),
+            HabitModel("1-11-1", "study", listOf(true, false, true)),
+            HabitModel("1-11-1", "study", listOf(true, false, true)),
+        )
+        val habitViewAdapter = HabitViewAdapter(habitLists)
+
+        binding.habitRecyclerView.adapter = habitViewAdapter
+        binding.habitRecyclerView.layoutManager = habitLayoutManager
+
+        return binding.root
+    }
 }
