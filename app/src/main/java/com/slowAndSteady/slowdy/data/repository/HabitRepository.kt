373a37506b94ad92cf34222727ddea6b4d21370d
Parkhi <parkhigoyal46@gmail.com>
2023-07-06 14:00:09 +0530

    added habit repository

---
+++ app/src/main/java/com/slowAndSteady/slowdy/data/repository/HabitRepository.kt
@@ -0,0 +1,19 @@
+package com.slowAndSteady.slowdy.data.repository
+
+import com.slowAndSteady.slowdy.data.dao.HabitDao
+import com.slowAndSteady.slowdy.data.entity.HabitEntity
+import kotlinx.coroutines.flow.Flow
+
+class HabitRepository(private val habitDao: HabitDao) {
+
+    fun getAllHabits(): Flow<List<HabitEntity>>{
+        return habitDao.getHabits()
+    }
+    suspend fun createAndUpdateHabit(habitEntity: HabitEntity){
+        habitDao.insertHabit(habitEntity)
+    }
+    suspend fun deleteHabit(habitId : Int){
+        habitDao.deleteHabitById(habitId)
+    }
+    
+}
