adb055e3d2df0db2111d6348534ad789f7cde48d
Parkhi <parkhigoyal46@gmail.com>
2023-07-09 20:39:48 +0530

    initialised dependency injection

--- app/src/main/java/com/slowAndSteady/slowdy/data/DatabaseHelper.kt
+++ app/src/main/java/com/slowAndSteady/slowdy/data/DatabaseHelper.kt
@@ -1,34 +1,14 @@
 package com.slowAndSteady.slowdy.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.slowAndSteady.slowdy.data.dao.HabitDao
 import com.slowAndSteady.slowdy.data.entity.HabitEntity
 
 @Database(entities = [HabitEntity::class],version = 1, exportSchema = false)
+@TypeConverters(HabitEntity.StreaksConverter::class)
 abstract class DatabaseHelper: RoomDatabase() {
     abstract fun habitDao(): HabitDao
-    companion object {
-        // Singleton prevents multiple instances of database opening at the
-        // same time.
-        @Volatile
-        private var INSTANCE: DatabaseHelper? = null
 
-        fun getDatabase(context: Context): DatabaseHelper {
-            // if the INSTANCE is not null, then return it,
-            // if it is, then create the database
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    DatabaseHelper::class.java,
-                    "word_database"
-                ).build()
-                INSTANCE = instance
-                // return instance
-                instance
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/slowAndSteady/slowdy/di/AppModule.kt
@@ -0,0 +1,38 @@
+package com.slowAndSteady.slowdy.di
+
+import android.content.Context
+import androidx.room.Room
+import com.slowAndSteady.slowdy.SlowdyApplication.Companion.appContext
+import com.slowAndSteady.slowdy.data.DatabaseHelper
+import com.slowAndSteady.slowdy.data.dao.HabitDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): DatabaseHelper{
+        return Room.databaseBuilder(
+            context,
+            DatabaseHelper::class.java,
+            "HabitDatabase"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHabitDao(
+        database : DatabaseHelper
+    ): HabitDao {
+        return database.habitDao()
+    }
+}
