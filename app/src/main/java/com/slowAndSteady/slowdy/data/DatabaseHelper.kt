ddad72692beeef3bbf275eb695954e28a5021532
Parkhi <parkhigoyal46@gmail.com>
2023-10-26 23:06:05 +0530

    added user dao to db

--- app/src/main/java/com/slowAndSteady/slowdy/data/DatabaseHelper.kt
+++ app/src/main/java/com/slowAndSteady/slowdy/data/DatabaseHelper.kt
@@ -4,11 +4,14 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.slowAndSteady.slowdy.data.dao.HabitDao
+import com.slowAndSteady.slowdy.data.dao.UserDao
 import com.slowAndSteady.slowdy.data.entity.HabitEntity
+import com.slowAndSteady.slowdy.data.entity.UserEntity
 
-@Database(entities = [HabitEntity::class],version = 1, exportSchema = false)
+@Database(entities = [HabitEntity::class, UserEntity::class],version = 1, exportSchema = false)
 @TypeConverters(HabitEntity.StreaksConverter::class)
 abstract class DatabaseHelper: RoomDatabase() {
     abstract fun habitDao(): HabitDao
+    abstract fun userDao(): UserDao
 
 }
