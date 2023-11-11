b73908758d980879866ae110eb749055d0ce53ff
Parkhi <parkhigoyal46@gmail.com>
2023-11-11 22:35:46 +0530

    integrated firestore

--- app/src/main/java/com/slowAndSteady/slowdy/data/entity/UserEntity.kt
+++ app/src/main/java/com/slowAndSteady/slowdy/data/entity/UserEntity.kt
@@ -4,10 +4,13 @@ import androidx.room.ColumnInfo
 import androidx.room.Database
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "user_table")
  class UserEntity (
-    @PrimaryKey(autoGenerate = false) val userID : String,
+    @PrimaryKey(autoGenerate = false) val userID : String = "",
     @ColumnInfo val userName  : String? = null,
     @ColumnInfo val userEmail : String? = null
-)
+) : BaseEntity(userID) {
+}
--- app/src/main/java/com/slowAndSteady/slowdy/data/repository/UserRepository.kt
+++ app/src/main/java/com/slowAndSteady/slowdy/data/repository/UserRepository.kt
@@ -1,19 +1,29 @@
 package com.slowAndSteady.slowdy.data.repository
 
+import com.slowAndSteady.slowdy.SlowdyApplication
 import com.slowAndSteady.slowdy.data.dao.HabitDao
 import com.slowAndSteady.slowdy.data.dao.UserDao
 import com.slowAndSteady.slowdy.data.entity.HabitEntity
 import com.slowAndSteady.slowdy.data.entity.UserEntity
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class UserRepository @Inject constructor(private val userDao: UserDao) {
+class UserRepository @Inject constructor(private val userDao: UserDao) : RemoteRepo {
+
+    override val collectionName: String
+        get() = "users"
 
     fun getUser(): Flow<UserEntity>{
         return userDao.getUser()
     }
+
     suspend fun createAndUpdateUser(userEntity: UserEntity){
         userDao.insertUser(userEntity)
+        SlowdyApplication.appScope.launch(Dispatchers.IO) {
+            collectionReference.document(userEntity.userID).set(userEntity)
+        }
     }
 
 }
