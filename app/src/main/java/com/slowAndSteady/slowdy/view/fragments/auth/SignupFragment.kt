52602fba32ed559900ffe26c2a9f025499bf7f7a
Parkhi <parkhigoyal46@gmail.com>
2023-11-12 00:02:20 +0530

    added sign up flow

--- app/src/main/java/com/slowAndSteady/slowdy/view/fragments/auth/SignupFragment.kt
+++ app/src/main/java/com/slowAndSteady/slowdy/view/fragments/auth/SignupFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -23,12 +24,14 @@ import com.slowAndSteady.slowdy.R
 import com.slowAndSteady.slowdy.databinding.FragmentSigninBinding
 import com.slowAndSteady.slowdy.databinding.FragmentSignupBinding
 import com.slowAndSteady.slowdy.view.activity.MainActivity
+import com.slowAndSteady.slowdy.viewModel.auth.AuthViewModel
 
 class SignupFragment : Fragment() {
     private lateinit var binding: FragmentSignupBinding
     private lateinit var navController: NavController
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
+    private val viewModel by activityViewModels<AuthViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -52,28 +55,43 @@ class SignupFragment : Fragment() {
         return binding.root
     }
 
-private val launcher =
-    registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val resultObtained = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-            if (resultObtained.isSuccessful) {
-                val account: GoogleSignInAccount? = resultObtained.result
-                val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
-                auth.signInWithCredential(credential).addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(requireContext(), "User already exists!!", Toast.LENGTH_SHORT).show()
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val resultObtained = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                if (resultObtained.isSuccessful) {
+                    val account: GoogleSignInAccount? = resultObtained.result
+                    val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+                    auth.signInWithCredential(credential).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            viewModel.createUser(
+                                account?.displayName.toString(),
+                                account?.email.toString(),
+                                task.result?.user?.uid.toString()
+                            )
+                            openHomeFragment()
+                            Toast.makeText(
+                                requireContext(),
+                                "User already exists!!",
+                                Toast.LENGTH_SHORT
+                            ).show()
 
-                    } else {
-                        Toast.makeText(requireContext(), "Failed!!", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(requireContext(), "Failed!!", Toast.LENGTH_SHORT).show()
+                        }
                     }
+                } else {
+                    // Handle sign-in failure
                 }
             } else {
-                // Handle sign-in failure
+                Toast.makeText(requireContext(), "Failed!!", Toast.LENGTH_SHORT).show()
             }
-        } else {
-            Toast.makeText(requireContext(), "Failed!!", Toast.LENGTH_SHORT).show()
         }
-    }
 
+    private fun openHomeFragment() {
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        startActivity(intent)
+        requireActivity().finish()
+    }
 
 }
